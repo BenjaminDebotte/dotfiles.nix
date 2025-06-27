7f29ebaafd8ce03a3878c43408a0174e734d795e
Benjamin Debotte <benjamin.debotte@gmail.com>
2025-06-27 22:13:25 +0200

    feat: try with packages null

--- home/user/hyprland.nix
+++ home/user/hyprland.nix
@@ -1,19 +1,16 @@
 { inputs, pkgs, ... }:
 
 {
-  hyprland.homeManagerModules.default {
-    wayland.windowManager.hyprland = {
-      enable = true;
-      # systemd.enable = true;
-      xwayland.enable = true;
-      # set the Hyprland and XDPH packages to null to use the ones from the NixOS module
-      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-      portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-      plugins = [
-        inputs.split-monitor-workspaces.packages.${pkgs.system}.split-monitor-workspaces
-      ];
-    };
-  }
-
+  wayland.windowManager.hyprland = {
+    enable = true;
+    # systemd.enable = true;
+    xwayland.enable = true;
+    # set the Hyprland and XDPH packages to null to use the ones from the NixOS module
+    package = null;
+    portalPackage = null;
+    plugins = [
+      inputs.split-monitor-workspaces.packages.${pkgs.system}.split-monitor-workspaces
+    ];
+  };
 
 }
