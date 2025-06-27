634ae4fb7740a915c936fe1239800ee8b38356b9
Benjamin Debotte <benjamin.debotte@gmail.com>
2025-06-27 22:42:05 +0200

    feat: despair hits again

--- home/user/hyprland.nix
+++ home/user/hyprland.nix
@@ -9,7 +9,6 @@
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     plugins = [
-      pkgs.hyprlandPlugins.hyprsplit
     ];
   };
 
--- system/modules/hyprland.nix
+++ system/modules/hyprland.nix
@@ -1,25 +1,24 @@
-{ config, inputs, pkgs, ... }:
+{ config, inputs, pkgs, pkgs-unstable, ... }:
 
 {
   # Full definition in users/hyprland.nix
   programs.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
-  environment.systemPackages = with pkgs; [
-    hyprpaper
-    kitty
-    libnotify
-    mako
-    qt5.qtwayland
-    qt6.qtwayland
-    swayidle
-    swaylock-effects
-    wlogout
-    wl-clipboard
-    wofi
-    waybar
+  environment.systemPackages =  [
+    pkgs.hyprpaper
+    pkgs-unstable.hyprlandPlugins.hyprsplit
+    pkgs.kitty
+    pkgs.libnotify
+    pkgs.mako
+    pkgs.qt5.qtwayland
+    pkgs.qt6.qtwayland
+    pkgs.swayidle
+    pkgs.swaylock-effects
+    pkgs.wlogout
+    pkgs.wl-clipboard
+    pkgs.wofi
+    pkgs.waybar
   ];
 }
