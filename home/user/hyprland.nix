f31f72fc61e19389a078404d5772b0a1e928ba24
Benjamin Debotte <benjamin.debotte@gmail.com>
2025-06-27 22:05:21 +0200

    feat: try without systemd

--- home/user/hyprland.nix
+++ home/user/hyprland.nix
@@ -4,7 +4,7 @@
 
   wayland.windowManager.hyprland = {
     enable = true;
-    systemd.enable = true;
+    # systemd.enable = true;
     xwayland.enable = true;
     # set the Hyprland and XDPH packages to null to use the ones from the NixOS module
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
