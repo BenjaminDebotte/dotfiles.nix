78d3bd6fb22a312aa8e63cfec1413ebc14b52baa
Benjamin Debotte <benjamin.debotte@gmail.com>
2026-04-25 16:35:27 +0200

    feat(perf): adds tuning for laptop

--- system/modules/services.nix
+++ system/modules/services.nix
@@ -7,6 +7,22 @@
     openssh.enable = true;
     spice-vdagentd.enable = true;
 
+    # Laptop specific
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC  = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_ENERGY_PERF_POLICY_ON_AC  = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_BOOST_ON_AC  = 1;
+        CPU_BOOST_ON_BAT = 0;
+      };
+    };
+
+    thermald.enable = true;
+    # end of laptop specific
+ 
     xserver = {
       enable = true;
       xkb.layout = "us";
